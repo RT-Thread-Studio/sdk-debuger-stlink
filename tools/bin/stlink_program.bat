4fad26a7cda28986dfb720cb263f53d1244497f9
armink <armink.ztl@gmail.com>
2020-05-08 16:06:24 +0800

    【增加】版本控制
    Signed-off-by: armink <armink.ztl@gmail.com>

---
+++ tools/bin/stlink_program.bat
@@ -0,0 +1,2 @@
+cd D:/RT-ThreadStudio/platform/ST-LINK/tools/bin
+D:/RT-ThreadStudio/platform/ST-LINK/tools/bin/STM32_Programmer_CLI.exe -c port=SWD mode=NORMAL -e all -d "D:\RT-ThreadStudio\workspace\l475ve\Debug\rtthread.elf" -hardRst -s
