4f675be238ef081dc1829235ce7682bb9fccca93
yaxing.chen <chenyaxing@rt-thread.com>
2021-03-22 16:36:07 +0800

    update to 1.6.0

--- tools/bin/stlink_program.bat
+++
@@ -1,2 +0,0 @@
-cd D:\STM32CubeIDE_1.3.0\123\tools\bin
-D:\STM32CubeIDE_1.3.0\123\tools\bin\STM32_Programmer_CLI.exe -c port=SWD mode=NORMAL -d "D:\eclipse-rcp-2018-12-R-win32-x86_64\eclipse\runtime-rttstudio.product\F411\Debug\rtthread.elf" -hardRst -s
